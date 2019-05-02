c2f84accd544aa4dca48cb06ed6993c1b100c570
pushprajkatiyar <pushprajkatiyar@gmail.com>
2019-05-02 12:33:25 +0530

    done with per second frame extraction and detection

---
+++ README.md
@@ -0,0 +1,11 @@
+# Person detection from live cctv video per second
+
+The project developed using TensorFlow to detect the no of people every second entering building gate. `work in progress`
+
+### Python Packages Needed
+
+* <a href='https://github.com/tensorflow/tensorflow'>Tensorflow</a><br>
+* <a href='https://github.com/skvark/opencv-python'>openCV</a><br>
+* you must have pip and python pre installed if not install them first.
+
+* Download and use, no license needed*
---
+++ video_detection_continue.py
@@ -0,0 +1,134 @@
+# ----------------------------------------------
+# --- Author         : Pushpraj Katiyar
+# --- Mail           : pushprajkatiyar@gmail.com
+# --- Date           : 23th April 2019
+# ----------------------------------------------
+import numpy as np
+import math
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import json
+import argparse
+
+from collections import defaultdict
+from io import StringIO
+import cv2
+
+if tf.__version__ < '1.13.0':
+    raise ImportError(
+        'Please upgrade your tensorflow installation to v1.13.* or later!')
+
+sys.path.insert(0, 'utils')
+import label_map_util
+import people_class_util as class_utils
+import visualization_utils as vis_util
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--path', required=True,
+#                     help='Path to the video')
+#
+# opt = parser.parse_args()
+
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2018_01_28'
+# MODEL_NAME = 'faster_rcnn_nas_lowproposals_coco_2017_11_08'
+#MODEL_NAME = 'faster_rcnn_resnet50_coco_2018_01_28'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used
+# for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = 'utils/person_label_map.pbtxt'
+
+NUM_CLASSES = 90
+cap = cv2.VideoCapture('flipkartad.mp4')
+#cap = cv2.VideoCapture('http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8')
+# checking if file exist or not and if yes then downloading specified model
+if not os.path.exists(MODEL_FILE):
+    opener = urllib.request.URLopener()
+    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+    tar_file = tarfile.open(MODEL_FILE)
+    for file in tar_file.getmembers():
+        file_name = os.path.basename(file.name)
+        if 'frozen_inference_graph.pb' in file_name:
+            tar_file.extract(file, os.getcwd())
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+# loading specified class/category description
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(
+    label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+# some helper code
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+
+# start detection here
+# For the sake of simplicity we will use only 2 images:
+# image1.jpg
+# image2.jpg
+# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+PATH_TO_TEST_IMAGES_DIR = 'test_images'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+i = 0
+success = True
+#detection of video
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    while success:
+      success, image_np = cap.read()
+      if not success:
+          print ('end of the video file...')
+          break
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      # Each box represents a part of the image where a particular object was detected.
+      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      # Each score represent how level of confidence for each of the objects.
+      # Score is shown on the result image, together with the class label.
+      scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      # Actual detection.
+      (boxes, scores, classes, num_detections) = sess.run(
+          [boxes, scores, classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+
+     # cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+      cv2.imshow('object detection', image_np)
+      print(">>>>framenumber>>>>>>>>>" + str(i))
+      if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
